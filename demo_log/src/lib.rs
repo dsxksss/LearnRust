80733e8f364b888a495de1c4fabceb24549e3619
dsxk <2546650292@qq.com>
2022-04-15 13:01:08 +0800

    add:rustdemo_log小项目实现写入log信息功能

--- demo_log/src/lib.rs
+++ demo_log/src/lib.rs
@@ -1,20 +1,30 @@
+use std::fmt::Display;
+use std::io::Error;
+
 pub struct SLog {
     pub log_level: ELogLevel,
+    pub pathname: String,
 }
 impl SLog {
     pub fn error(&self, msg: &str) -> () {
         if self.log_level.get_level() >= SLog::get_level(ELogLevel::Error) {
             println!("[Error]:\u{001b}[31m {} \u{001b}[0m", msg);
+            write_file(&self.pathname, format!("[Error]:{}", String::from(msg)))
+                .expect("Error write error!");
         }
     }
     pub fn warning(&self, msg: &str) -> () {
         if self.log_level.get_level() >= SLog::get_level(ELogLevel::Warning) {
             println!("[Warning]:\u{001b}[33m {} \u{001b}[0m", msg);
+            write_file(&self.pathname, format!("[Warning]:{}", String::from(msg)))
+                .expect("Warning write error!");
         }
     }
     pub fn info(&self, msg: &str) -> () {
         if self.log_level.get_level() >= SLog::get_level(ELogLevel::Info) {
             println!("[Info]:\u{001b}[34m {} \u{001b}[0m", msg);
+            write_file(&self.pathname, format!("[Info]:{}", String::from(msg)))
+                .expect("Info write error!");
         }
     }
     pub fn get_level(level: ELogLevel) -> u8 {
@@ -40,4 +50,30 @@ impl ELogLevel {
             ELogLevel::Info => 2,
         }
     }
+}
+
+fn get_time() -> String {
+    use chrono::prelude::*;
+    let fmt = "%Y-%m-%d %H:%M:%S";
+    let now = Local::now().format(fmt);
+    format!("{}", now)
+}
+
+fn write_file<T: Display>(pathname: &str, writedata: T) -> Result<(), Error> {
+    use std::fs::{File, OpenOptions};
+    use std::io::{prelude::*, ErrorKind};
+
+    File::open(pathname).unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create(pathname).unwrap_or_else(|error| {
+                panic!("Error creating file : {:?}", error);
+            })
+        } else {
+            panic!("Error opening file : {:?}", error);
+        }
+    });
+
+    let mut write_file = OpenOptions::new().append(true).open(pathname)?;
+    write_file.write_fmt(format_args!("{}\t\t[{}]\n", writedata, get_time()))?;
+    Ok(())
 }
--- demo_log/src/main.rs
+++ demo_log/src/main.rs
@@ -1,9 +1,10 @@
 use demo_log::{ELogLevel, SLog};
 fn main() {
     let log = SLog {
-        log_level: ELogLevel::Warning,
+        pathname: String::from("rustDemo.log"),
+        log_level: ELogLevel::Info,
     };
     log.error("I am a Error msg");
-    log.warning(&String::from("I am a Error msg"));
-    log.info(&String::from("I am a Error msg"));
+    log.warning(&String::from("I am a Warning msg"));
+    log.info(&String::from("I am a Info msg"));
 }
